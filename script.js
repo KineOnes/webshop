d2dc17d371e0d66025f4e4eae2a593730b05c34c
KineOnes <kine.verhaaf@gmail.com>
2023-04-06 09:35:02 +0200

    added fucntion to fetch from url

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const baseUrl = "http://hello-world.local//wp-json/wc/store/products";
 async function getProducts(url){
     const response = await fetch(url);
     const products = await response.json();
-    console.log(products);
+    products.forEach(function(product){
+        productContainer.innerHTML += `
+        <div class="product"><h2>${product.name}</h2></div>
+        <div class="product-image" style="background-image:url('${product.images[0].src}')</div>
+        <div class="product"><h2>${product.price_html}</h2></div>
+        `
+    })
 }
 
 getProducts(baseUrl);

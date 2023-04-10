db5a30539cdada0263d951a446c544f6ed7fad4a
KineOnes <kine.verhaaf@gmail.com>
2023-04-10 21:15:34 +0200

    adjustet porducts in woocommerse, added featured product in woocommerse

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ async function getProducts(url){
     const products = await response.json();
     products.forEach(function(product){
         productContainer.innerHTML += `
-        <div class="product"><h2>${product.name}</h2></div>`;
+            <div class="product"><h2>${product.name}</h2></div>
+            <div class="product"><h2>${product.description}</h2></div>
+            <div class="product"><h2>${product.add_to_cart}</h2></div>`;
     })
     
 }

070d2aa1756b119ee92e13d456147cd8253f040b
KineOnes <kine.verhaaf@gmail.com>
2023-04-20 08:10:09 +0200

    added duplicated files and folders from Rainydays repo

---
+++ featured.js
@@ -0,0 +1,16 @@
+const baseUrl = "http://hello-world.local/wp-json/wc/store/products?featured=true";
+const productContainer = document.querySelector(".products");
+
+async function getProducts(url){
+    const response = await fetch(url);
+    const products = await response.json();
+    products.forEach(function(product){
+        productContainer.innerHTML += `
+            <div class="category-item"><h2>${product.name}</h2>
+            <h2>${product.description}</h2>
+            <h2>${product.add_to_cart}</h2></div>`;
+    })
+    
+}
+
+getProducts(baseUrl);
